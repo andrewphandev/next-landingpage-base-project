441ebda17486b9284d4c011d04e6d692e071aacb
Andrew <andrewphandev@gmail.com>
2022-02-16 16:51:14 +0700

    change img thumnail

--- next-seo.config.js
+++ next-seo.config.js
@@ -3,22 +3,24 @@ const defaultSEOConfig = {
   title: process.env.NEXT_PUBLIC_SITE_NAME,
   titleTemplate: '%s',
   defaultTitle: process.env.NEXT_PUBLIC_SITE_NAME,
-  description: 'Next.js + chakra-ui + TypeScript template',
+  description:
+    'DNF. Worldwide based custom software development & consulting company focusing on web, mobile, desktop & embedded software development. ',
   canonical: process.env.NEXT_PUBLIC_SITE_URL,
   openGraph: {
     url: process.env.NEXT_PUBLIC_SITE_URL,
     title: process.env.NEXT_PUBLIC_SITE_NAME,
-    description: 'Next.js + chakra-ui + TypeScript template',
+    description:
+      'DNF. Worldwide based custom software development & consulting company focusing on web, mobile, desktop & embedded software development. ',
     images: [
       {
-        url: 'https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250',
-        alt: `${ process.env.NEXT_PUBLIC_SITE_URL} og-image`,
+        url: 'https://res.cloudinary.com/andrewphan/image/upload/v1645004991/dnf-soft/image-4_ugqjei.png',
+        alt: `${process.env.NEXT_PUBLIC_SITE_URL} og-image`,
       },
     ],
     site_name: process.env.NEXT_PUBLIC_SITE_NAME,
   },
   twitter: {
-    handle: '@sozonome',
+    handle: '@dnfsoft',
     cardType: 'summary_large_image',
   },
 }
